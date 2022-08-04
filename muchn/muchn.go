bd0060ba0837087e45a7b3cc266268a3597c285e
Ryan Shaw-Harrison <ryan.shaw-harrison@protonmail.com>
2022-08-04 21:25:03 +0100

    Added additional commentary.

--- muchn/muchn.go
+++ muchn/muchn.go
@@ -13,16 +13,21 @@ import (
 
 func main() {
 	sctmp := ""
-	scale := 5 // default scale is 3
+	scale := 5 // default scale is 5
 
 	// scale is only changed if any user args exist
+	// otherwise remains the default as initially declared
 	if len(os.Args) > 1 {
 		sctmp = os.Args[1]
 		scale, _ = strconv.Atoi(sctmp)
 	}
 
+	// works line by line
 	for line := 0; line < scale; line++ {
-		fmt.Print("N")
+		fmt.Print("N") // first character of the line should always be an N
+
+		// calculates where the N should be printed in the current line
+		// to connect the first and last line of Ns diagonally
 		for column := 1; column < scale-1; column++ {
 			switch {
 			case column == line:
@@ -31,6 +36,6 @@ func main() {
 				fmt.Print(" ")
 			}
 		}
-		fmt.Println("N")
+		fmt.Println("N") // last character of the line should always be an N, then a new line
 	}
 }
